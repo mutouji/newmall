8d2b3937a527287a8e3bee8f760c8ddebc076752
mutouji <59022665@qq.com>
2017-12-31 18:05:24 +0800

    first commit

---
+++ taotao-manager-interface/src/main/java/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<dependency>
+		    <groupId>com.taotao</groupId>
+		    <artifactId>taotao-manager-pojo</artifactId>
+		    <version>0.0.1-SNAPSHOT</version>
+		</dependency>
+	}
+}
+
